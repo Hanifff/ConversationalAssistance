ed5fb801e418c33185d0d15a04cfa2dff3a45a4f
Hanif <mohamad.h@hotmail.no>
2021-11-11 22:00:29 +0100

    base line method done

--- baseline_method.py
+++ baseline_method.py
@@ -45,49 +45,39 @@ class BaseLine(IndexManagement):
             query_terms.append(t["token"])
         return query_terms
 
-    def score_term(self, index_name: str, filepath: str) -> Dict[str, List[str]]:
-        """We rank queries using the default BM25 in elastisc search module.
-            , query_ids: List[str], all_queries: Dict[str, str], 
+    def score_term(self, index_name: str, filepath: str) -> None:
+        """Ranks passages using the default BM25 in elastisc search module.
+            The result is written in a text file with trec_eval format requirements.
 
         Args:
             index_name: Name of indexing instance.
             query_ids: List of query Ids.
             all_queries: A key value set of all queries.
-
-        Returns:
-            A set of ranked queries.
         """
-        rankings = {}
         data = {}
-        Q_0 = 0
+        Q_0 = str(0)
         with open(file=filepath) as f:
             data = json.load(f)
+        f.close()
+        bm25_result = open("./data/results/bm25_results.txt", "w")
         for i in range(len(data)):
-            print(data[i]["turn"])
+            TOPICID = str(data[i]["number"])+'_'
+            for turn in data[i]['turn']:
+                TOPICID_TURNID = TOPICID+str(turn["number"])
+                query = turn['manual_rewritten_utterance']
+                # First-pass retrieval
+                query_terms = self.analyze_query(
+                    query, "body", index_name)
 
-        """ for hit in hits:
-            TOPICID_TURNID = hit["number"]+"_"+hit["number"]["number"]
-            # Q_O
-            passage_identifier = self.collection_name+query_id
-            # rank
-            # score
+                hits = self.es_cli.search(
+                    index=index_name, q=" ".join(query_terms), _source=True, size=100
+                )["hits"]["hits"]
+                for i in range(len(hits)):
+                    passage_id = self.collection_name + hits[i]["_id"]
+                    bm25_result.write(TOPICID_TURNID+' '+Q_0 + ' '+passage_id +
+                                      ' '+str(i+1)+' '+str(round(hits[i]["_score"], 1))+' '+"Team-011"+'\n')
 
-        for i, query_id in enumerate(query_ids):
-            print(
-                "Processing query {}/{} ID {}".format(
-                    i + 1, len(query_ids), query_id
-                )
-            )
-            # First-pass retrieval
-            query_terms = self.analyze_query(
-                self.es_cli, all_queries[query_id], "body", index=index_name
-            )
-
-            hits = self.es_cli.search(
-                index=index_name, q=" ".join(query_terms), _source=True, size=1
-            )["hits"]["hits"]
-            rankings[query_id] = [hit["_id"] for hit in hits]
-        return rankings """
+        bm25_result.close()
 
 
 if __name__ == "__main__":
